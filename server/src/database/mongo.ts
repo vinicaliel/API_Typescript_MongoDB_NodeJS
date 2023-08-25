d979d668cc6ca28b0a0b79fd107bbb1b916d85b3
vinicius caliel nunes passos <caliel1023@yahoo.com>
2023-08-25 16:29:05 -0300

    feat dotenv

---
+++ server/src/database/mongo.ts
@@ -0,0 +1,10 @@
+export const mongoClient = {
+
+    async connect(): Promise<void>{
+
+        const url = process.env.MONGOSB_URL
+        const username = process.env.MONGODB_USER
+        const password =  process.env.MONGODB_PASSWORD
+
+    }
+}
